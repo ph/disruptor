de665acd7275e56ac43c67ca7c97598b32439cca
Pier-Hugues Pellerin <phpellerin@gmail.com>
2014-12-24 01:09:03 -0500

    testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 require 'minitest/autorun'
 require 'minitest/reporters'
+require 'coveralls'
+Coveralls.wear!
 
 Minitest::Reporters.use!(Minitest::Reporters::DefaultReporter.new({ color: true }))
