d2785540117a0ead330fa7473c40dc61f875620d
Pier-Hugues Pellerin <phpellerin@gmail.com>
2014-12-24 01:16:10 -0500

    testing coverals

---
+++ lib/disruptor/ring_buffer.rb
@@ -0,0 +1,7 @@
+module Disruptor
+  class RingBuffer
+    def test
+      2
+    end
+  end
+end
--- test/disruptor/test_ring_buffer.rb
+++ test/disruptor/test_ring_buffer.rb
@@ -1,7 +1,11 @@
 require 'minitest/autorun'
+require 'disruptor/ring_buffer'
 
-class TestRingBugger < Minitest::Test
-  def test_addition
-    assert_equal 1, 1
+module Disruptor
+  class TestRingBugger < Minitest::Test
+    def test_addition
+      b = RingBuffer.new
+      assert_equal b.test, 2
+    end
   end
 end
